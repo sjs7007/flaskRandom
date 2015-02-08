e38812ba9b52ac5f5481cf73bbcbacc1b539d7c0
Saravanan <sjs7007@gmail.com>
2015-02-08 10:46:46 +0530

    2.1

--- megaTut/app/views.py
+++ megaTut/app/views.py
@@ -3,5 +3,15 @@ from app import app #import app variable defined in init
 @app.route('/')
 @app.route('/index')
 def index():
-	return "<h1>Hello, World!</h1>"
+	user={'nickname': 'shinchan'} #fake user
+	return '''
+<html>
+	<head>
+		<title>Home Page</title>
+	</head>
+	<body>
+		<h3>Hello, ''' + user['nickname'] + '''</h3>
+	</body>
+</html>
+	'''
 
