24f537e2ea462758b0fb1981915b65ddd478363b
Saravanan <sjs7007@gmail.com>
2015-02-08 11:26:31 +0530

    2.5 - tutorial 2 done

--- megaTut/app/views.py
+++ megaTut/app/views.py
@@ -7,11 +7,11 @@ def index():
 	user={'nickname': 'shinchan'} #fake user
 	posts = [ #fake array of post
 		{
-			'author': {'nickname': 'shin'},
+			'author': {'nickname': 'shintest2'},
 			'body': 'I made rice cake today!'
 		},
 		{
-			'author': {'nickname': 'harry'},
+			'author': {'nickname': 'harrytest2'},
 			'body': 'existential crisis.'
 		}
 	]
