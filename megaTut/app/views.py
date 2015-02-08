92105ec9e1e9f0a9ed7667c840eca76211ab86ec
Saravanan <sjs7007@gmail.com>
2015-02-08 11:16:32 +0530

    2.4

--- megaTut/app/views.py
+++ megaTut/app/views.py
@@ -5,4 +5,14 @@ from app import app #import app variable defined in init
 @app.route('/index')
 def index():
 	user={'nickname': 'shinchan'} #fake user
-	return render_template('index.html',user=user)
+	posts = [ #fake array of post
+		{
+			'author': {'nickname': 'shin'},
+			'body': 'I made rice cake today!'
+		},
+		{
+			'author': {'nickname': 'harry'},
+			'body': 'existential crisis.'
+		}
+	]
+	return render_template('index.html',user=user,posts=posts)
